304b58fa98e854a05be583b9e5a766de427d5970
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-09-14 10:27:01 +0000

    working on game play

--- src/Dom.js
+++ src/Dom.js
@@ -10,7 +10,7 @@ const DOM = (() => {
         newGame.player2 = player(true);
        
     }
-
+//push ship to board
     function throughBoard(highLIghted){
         let box = document.querySelectorAll(".box");
         for(let j = 0; j < box.length ; j++){
@@ -95,13 +95,11 @@ const DOM = (() => {
             elem.addEventListener("click", () => {
                 let player = newGame.player1
                 let shipNo = newGame.player1.currShip
-                let currShipLen = newGame.player1.playerShips[shipNo].shipLength
                 let yAxis = parseInt(elem.getAttribute("point")[0], 10);
                 let xAxis = parseInt(elem.getAttribute("point")[2], 10);
                 let all = [];
-                if(shipNo > player.playerShips.length - 1){
-                    return
-                }
+            
+                let currShipLen = newGame.player1.playerShips[shipNo].shipLength
                 if(newGame.gameBoard1.isYaxis){
                     for(let i = 0 ; i < currShipLen ; i++){    
                         all.push(throughBoard(`${yAxis+i},${xAxis}`))
@@ -112,24 +110,33 @@ const DOM = (() => {
                         all.push(throughBoard(`${yAxis},${xAxis + i}`))                            
                     }           
                 }
+                console.log(all)
                 if(all.indexOf(undefined) !== -1 ) return;
                 if(isSpaceOccupied(all)) return
                 all.forEach(elem => {   
-                  newGame.gameBoard1.shipGridSpot(player.name, currShipLen,elem.getAttribute("point")[0], elem.getAttribute("point")[2], newGame.gameBoard1.isYaxis)   
+                    console.log(elem.getAttribute("point"))
+                  newGame.gameBoard1.shipGridSpot(player.name,elem.getAttribute("point")[0], elem.getAttribute("point")[2])   
                     elem.style.borderColor = "red"
                 })
                 newGame.player1.currShip++;    
                     console.log(newGame.gameBoard1)
                     console.log(newGame.gameBoard1.allspot)
-                 //   if(shipNo)
+                    if(shipNo >= player.playerShips.length - 1){
+                        renderGamePlay1();
+                        renderGamePlay2();
+                        return
+                    }
             })
         })
     } 
 
+    // create players and board
     function setBoards(){
         newGame.gameBoard1 = gameBoards();
         newGame.gameBoard2 = gameBoards();    
     }
+
+    // render UIs
 
     function renderShipsOnBoard(){
         let strtGme = document.querySelector(".start_game");  
@@ -142,10 +149,54 @@ const DOM = (() => {
        setShipListener()
     }
 
-    function renderGamePlay(){
-        document.querySelector
+   
+
+    function getPLaceshipsP1(input){
+        let box = document.querySelectorAll("div.first > .box")
+        box.forEach(elem => {
+            if(elem.getAttribute("point") === input){
+                elem.style.backgroundColor = "green"
+            }
+        })
+      
+    }   
+
+    function getPLaceshipsP2(input){
+        let box = document.querySelectorAll("div.second > .box")
+        box.forEach(elem => {
+            if(elem.getAttribute("point") === input){
+                elem.style.backgroundColor = "green"
+            }
+        })
+      
+    }   
+
+    function renderGamePlay1(){
+        let games = document.querySelector(".games");
+        let start = document.querySelector(".start_game")
+        start.innerHTML = "";
+        games.innerHTML = "";
+        renderBoard();
+        let grid = document.querySelector(".grid-box")
+        grid.classList.add('first');
+        newGame.gameBoard1.allspot.forEach(elem => {
+            getPLaceshipsP1(`${elem.y_axis},${elem.x_axis}`)
+          
+         })
     }
 
+    function renderGamePlay2(){
+        let games = document.querySelector(".games");
+        renderBoard();
+        games.lastElementChild.classList.add("second")
+        let box = document.querySelectorAll("div.second > .box")
+        newGame.gameBoard2.allspot.forEach(elem => {
+            getPLaceshipsP2(`${elem.y_axis},${elem.x_axis}`)
+          
+         })
+    }
+
+   
     function renderBoard(){
         let width = 10;
         let height = 10;
@@ -163,31 +214,50 @@ const DOM = (() => {
         games.append(space)
     }
 
-    function setCompShip(shipno) {
-        let t_f =[true, false]
-        let player = newGame.player2;
-        let shipNo = shipno;
-        if(shipNo > player.playerShips.length - 1) return;
-        let currShip = newGame.player2.playerShips[shipNo];
-        let StaticRand = Math.floor(Math.random() * 10 );
-        let useStaticRand = Math.floor(Math.random() * (10 - currShip.shipLength))
-        newGame.gameBoard2.isYaxis = t_f[Math.floor(Math.random()*2)];
-        if(newGame.gameBoard2.isYaxis){
-            for(let i = 0; i < currShip.shipLength; i++){ 
-                newGame.gameBoard2.shipGridSpot(currShip.name, useStaticRand + i, StaticRand)
-            }
+    function setCompShip(num) {
+    let statics = randNum(1,9)
+    if(num >= newGame.player2.playerShips.length) return
+    let staticLIst = statics[num];
+    let bool = [true, false];
+    let isYaxis = bool[Math.floor(Math.random() * 2)];
+    let player = newGame.player2;
+    let currShip = player.playerShips[num];
+    let change = Math.floor(Math.random() * (10 - currShip.shipLength)) 
+    for(let i = 0; i < currShip.shipLength; i++){
+        if(isYaxis){
+            newGame.gameBoard2.shipGridSpot(player.name,change + i, staticLIst) 
         }
-        if(!newGame.gameBoard2.isYaxis){
-            for(let i = 0; i < currShip.shipLength; i++){ 
-                newGame.gameBoard2.shipGridSpot(currShip.name,  StaticRand, useStaticRand + i)
-            }
+        if(!isYaxis){
+            newGame.gameBoard2.shipGridSpot(player.name,staticLIst,change + i) 
         }
-        shipNo++
-        setCompShip(shipNo)
+     }
+     num++
+     setCompShip(num)
+     console.log(newGame)
+      
     }
+    
+    function randNum (){
+        let randNum = []
+        let pull = [1,2,3,4,5,6,7,8,9];
+        function checkNo(count, length){
+            let counter = count;
+            let size = length;
+            let rand = Math.floor(Math.random() * size);
+            if(counter > 5) return
+            randNum.push(pull[rand])    
+            pull.splice(rand,1)
+            counter++
+            size--
+            checkNo(counter, size)
+            
+        }
+      checkNo(1,9)
+      return randNum
+    }
+ 
 
-
-
+//listeners
     function listeners(){
         let btnStrt = document.querySelector(".btn_start")
         let playerName = document.querySelector(".player_name");
@@ -197,7 +267,12 @@ const DOM = (() => {
             setBoards("jake")
             renderShipsOnBoard();
             setCompShip(0);
+            console.log(newGame.gameBoard2)
         })   
+    }
+
+    function playGame(){
+        
     }
 
     function init(){
